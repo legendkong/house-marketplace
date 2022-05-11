1799b7350dc9b046b91207c97478fa8e2ca3df60
Jonathan Kong <jonathankong96@hotmail.com>
2022-05-12 00:47:34 +0800

    Implemented Navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Explore from './pages/Explore'
 import Offers from './pages/Offers'
 import SignUp from './pages/SignUp'
@@ -16,11 +17,11 @@ function App() {
         <Route path='/offers' element={<Offers />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
         <Route path='/profile' element={<Profile />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
       </Routes>
+      <Navbar />
     </Router>
-      {/* Navbar */}
     </>
   );
 }
